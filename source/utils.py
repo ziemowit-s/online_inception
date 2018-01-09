0539fb25627efcb5d5ff5d23a8708195b151c782
Ziemowit S <ziemowit.slawinski@gmail.com>
2018-01-09 21:07:28 +0100

    changed paths

--- source/utils.py
+++ source/utils.py
@@ -4,10 +4,10 @@ from tensorflow.python.platform import gfile
 
 class Props:
     image_dir = '../flower_photos'
-    model_dir = 'inception_dir'
-    bottleneck_dir = 'tmp/bottleneck'
+    model_dir = '../data/inception_dir'
+    bottleneck_dir = '../data/tmp/bottleneck'
     final_tensor_name = 'final_result'
-    summaries_dir = 'tmp/retrain_logs'
+    summaries_dir = '../data/tmp/retrain_logs'
     architecture = 'inception_v3'
     how_many_training_steps = 4000
     train_batch_size = 100
@@ -15,10 +15,10 @@ class Props:
     eval_step_interval = 10  # How often to evaluate the training results.
     validation_batch_size = 100
     intermediate_store_frequency = 0  # How many steps to store intermediate graph. If "0" then will not store.\
-    intermediate_output_graphs_dir = 'tmp/intermediate_graph/'
+    intermediate_output_graphs_dir = '../data/tmp/intermediate_graph/'
     print_misclassified_test_images = False  # Whether to print out a list of all misclassified test images
-    output_graph = 'tmp/output_graph.pb'
-    output_labels = 'tmp/output_labels.txt'
+    output_graph = '../data/tmp/output_graph.pb'
+    output_labels = '../data/tmp/output_labels.txt'
     learning_rate = 0.01
     flip_left_right = False  # Whether to randomly flip half of the training images horizontally
     random_crop = 0  # A percentage determining how much of a margin to randomly crop off the training images.\
